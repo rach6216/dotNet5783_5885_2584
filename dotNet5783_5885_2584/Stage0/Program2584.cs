c294e33a1f5cd82360e6e86960985d4a02af4c22
rach6216 <rach0556776216@gmail.com>
2022-10-03 16:00:51 +0300

    level 5 first partner

---
+++ dotNet5783_5885_2584/Stage0/Program2584.cs
@@ -0,0 +1,20 @@
+﻿namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome2584();
+            Welcome5885();
+            Console.ReadKey();
+        }
+
+        private static void Welcome2584()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+        static partial void Welcome5885();
+    }
+}
