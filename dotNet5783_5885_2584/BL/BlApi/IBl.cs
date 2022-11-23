f49de8549cefdda5b551016b0aded5db2a0349f6
rach6216 <rach0556776216@gmail.com>
2022-11-23 14:19:52 +0200

    IBl

---
+++ dotNet5783_5885_2584/BL/BlApi/IBl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi
+{
+    public interface IBl
+    {
+        /// <summary>
+        /// cart interface
+        /// </summary>
+        public ICart Cart { get; }
+        /// <summary>
+        /// product interface
+        /// </summary>
+        public IProduct Product { get; }
+        /// <summary>
+        /// order interface
+        /// </summary>
+        public IOrder Order { get; }
+    }
+}
